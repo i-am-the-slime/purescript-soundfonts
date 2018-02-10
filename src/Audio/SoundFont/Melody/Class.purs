b9883e3b2bd1b66a7b64d61b106c14b0f508048a
John Watson <john.watson@gmx.co.uk>
2018-02-10 11:43:03 +0000

    Improve documentation of Playable class

--- src/Audio/SoundFont/Melody/Class.purs
+++ src/Audio/SoundFont/Melody/Class.purs
@@ -3,7 +3,14 @@ module Audio.SoundFont.Melody.Class where
 -- | A typeclass representing those sound sources that can be played
 -- | by a soundfont player.  The InstrumentChannels argument is intended
 -- | for use with those sources which need to establish the MIDI channel
--- | of each note from the MIDI instruments that are in scope.
+-- | of each note from the MIDI instruments that are in scope. If it is
+-- | not required, users can supply the value empty.
+
+-- At the time of writing, PureScript does not allow default implementations
+-- within tyoeclass definitions.  If we were to have this, we could do away
+-- with the constraint of requiring instances to supply a dummy value for
+-- InstrumentChannels when it isn't needed by providing two method signatures
+-- with toMelody being implemented by (say) toMelodyWithLookup.
 
 import Audio.SoundFont (InstrumentChannels)
 import Audio.SoundFont.Melody (Melody, PMelody(..))
